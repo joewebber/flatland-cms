259215a09dc74898c984320d8ef31b90c9220e32
Joe Webber <joe.webber@gmail.com>
2015-02-27 21:08:51 +0000

    First commit

---
+++ app/controllers/Generator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Flatland;
+
+final class Generator {
+
+    public function __construct()
+    {
+
+    }
+
+    protected function _getMenus()
+    {
+
+    }
+
+    protected function _getPages()
+    {
+
+    }
+
+    protected function _getPageTitle()
+    {
+
+    }
+
+    protected function _getMetaContent()
+    {
+
+    }
+
+    protected function _renderHead()
+    {
+
+    }
+
+    protected function _renderBody()
+    {
+
+    }
+
+}
